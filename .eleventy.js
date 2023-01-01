fdfb9824214805c4220e6abdb74dde27a19fcda0
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-01-01 17:59:05 -0500

    fixed date format issue with filter in .eleventy.js. applied to blog.njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy('./src/styles.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.from.JSDate(dateObj).toLocalString(DateTime.DATE_MED);
+    })
 
     return {
         dir: {
