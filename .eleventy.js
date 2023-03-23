0c140d1e402b09e3026f9578fa4b572890e7104d
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-03-23 14:04:45 -0400

    updated .eleventy.js for prism code syntax. create dir themes create prism.css add code via copypaste

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy('./src/styles.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPlugin(syntaxHighlight);
 
+    // eleventyConfig.addPlugin is from: https://www.11ty.dev/docs/plugins/syntaxhighlight/
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
       });
