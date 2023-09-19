87dbf0ebebccc3fa237d0de7a28715ed37291e40
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-09-19 13:12:16 -0400

    install rss plugin via npm. add code to config: eleventry.js. create feed.njk add boilerplate json to it.

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy('./src/styles.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(pluginRss);
 
     // eleventyConfig.addPlugin is from: https://www.11ty.dev/docs/plugins/syntaxhighlight/
     eleventyConfig.addFilter("postDate", (dateObj) => {
